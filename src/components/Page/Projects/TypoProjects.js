52bcadc0216f7c683edd6b1a7862442c610418a2
YoniB1995 <yonatansamfisher@gmail.com>
2021-09-22 15:50:18 +0300

    updated

--- src/components/Page/Projects/TypoProjects.js
+++ src/components/Page/Projects/TypoProjects.js
@@ -1,18 +1,18 @@
 
 const ECOMMERCE_PROJECT = "https://serving.photos.photobox.com/91359195240512a8760900afa737cff707c55ac61ae0e2b637297fafad42eb24ce563666.jpg";
-const GKI_GROUP_PROJECT = "https://serving.photos.photobox.com/22952542fde28b836bcc16df59c1e9083de12fd944e1c2a8be2beb4e340fe5adaf38c4f2.jpg";
+const HOURS_COMPANY_PROJECT = "https://serving.photos.photobox.com/22952542fde28b836bcc16df59c1e9083de12fd944e1c2a8be2beb4e340fe5adaf38c4f2.jpg";
 const NETLIX_PROJECT = "https://serving.photos.photobox.com/64651908b8f6caff688cc8c95125234a818dcddffcf428f6811bcc256c43c1764efcbc69.jpg";
 const COVID19_PROJECT = "https://serving.photos.photobox.com/49880030955233e4e6870fd09122f2b218c50b7969b75ab8fa7e6e108d2a759c88140862.jpg";
 const PORTFOLIO_PROJECT = "https://serving.photos.photobox.com/16680903252e2f3264674a728cd02348a94c5beac8f38929e92b95321f83b318eae11369.jpg";
 const IN_PROGRESS = "https://serving.photos.photobox.com/355609936caea0c764e46b3737f999a905bd00919f20b28e1d7485695d43957b3b463d99.jpg";
 
-const images = [ECOMMERCE_PROJECT,GKI_GROUP_PROJECT,NETLIX_PROJECT,COVID19_PROJECT,PORTFOLIO_PROJECT,IN_PROGRESS];
+const images = [ECOMMERCE_PROJECT,HOURS_COMPANY_PROJECT,NETLIX_PROJECT,COVID19_PROJECT,PORTFOLIO_PROJECT,IN_PROGRESS];
 
 //  עברית
     const titles = [" אפליקציית חנות איקומרס ","אתגר פולסטאק 72שעות ","פרוייקט פרונט אתר נטפליקס","אתר מידע בנושא קורונה","פרוייקט תבנית פורטפוליו פשוטה","בקרוב"]
     const description = [
         "פרוייקט שמאופיין בתיקיות צד שרת ולקוח,שימוש באוטנטיקציה ושמירת מידע בדאטה בייס -ורידקס",
-        "פרוייקט 72 שעות משימת בית מחברת  בשילוב פלטפורמת GKI Group  פיירבייס רישום משתמשים ורידקס ",
+        "פרוייקט 72 שעות משימת בית מחברת  בשילוב פלטפורמת Company Group  פיירבייס רישום משתמשים ורידקס ",
         "פרוייקט אתר סרטים שנוצר עם שימוש בפלטפורמת ריאקט , כולל בתוכו שימוש ב''ראוטינג'' ומשיכת מידע משרתים נוספים",
         "הזרמת מידע בנושא קורונה ממס' שרתי אייפיאי, שימוש בבוטסטראפ",
     "פרוייקט פורטפויליו , שימוש בסטייל קומפוננטס וקונטקס - מעולה לריאיונות עבודה",
@@ -26,7 +26,7 @@ const images = [ECOMMERCE_PROJECT,GKI_GROUP_PROJECT,NETLIX_PROJECT,COVID19_PROJE
     const titlesEnglish = ["eCommerce Store Mern Stack App","72 Hours Project Challenge","Netflix Clone Project","Covid-19 API Project","Portfolio Project","On Progress"]
     const descriptionEnglish = [
         "Featuring server & client sides -User Auth & Users DB & Google Login & Redux",
-        "72 Hours Project Challenge made for GKI-Group,Mini eCommerce App with Redux & Firebase Auth. ",
+        "72 Hours Project Challenge made for Company,Mini eCommerce App with Redux & Firebase Auth. ",
         "Netflix Design Website Featuring client side and CSS , Routing Method and Filtering -Responsive",
         "Provides Live information about cases,deathes and news around the world. Built with React & Bootstrap", 
     "Free for use Protfolio template , Using styled components and context method. Great for CV applications.","Mern Stack Portfolio Featuring Sass...Soon"]
@@ -35,7 +35,7 @@ const images = [ECOMMERCE_PROJECT,GKI_GROUP_PROJECT,NETLIX_PROJECT,COVID19_PROJE
 
         const githubLinks = [
     "https://github.com/YoniB1995/BuyNSell-eCommerce-App",
-    "https://github.com/YoniB1995/gk-group-hw-project",
+    "https://github.com/YoniB1995/ecommerce-hw-project",
     "https://github.com/YoniB1995/netflix-clone-project",
     "https://github.com/YoniB1995/covid-daily-application",
     "https://github.com/YoniB1995/portfolio-app-cv",
